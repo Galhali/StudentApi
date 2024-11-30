24994eae10d85d3863be8dd3d055c7b864be6e50
Marcin Zawadzki <nicram01@tlen.pl>
2024-11-30 18:11:13 +0100

    create StudentRepository

---
+++ StudentApi/Repositories/StudentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApi.Data;
+using StudentApi.Models;
+
+namespace StudentApi.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly AppDbContext context;
+
+        public StudentRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            context.Students.Add(student);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStudentAsync(Guid id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if(student != null)
+            {
+                context.Students.Remove(student);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            return await context.Students.ToListAsync();
+        }
+
+        public async Task<Student> GetStudentByIdAsync(Guid id)
+        {
+            return await context.Students.FindAsync(id) ?? new Student();
+        }
+
+        public async Task UpdateStudentAsync(Student student)
+        {
+            context.Entry(student).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- StudentApi/obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
+++ StudentApi/obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890f7a0ddfd380c07a3d5ad7b7d3d3ddf293e9f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
